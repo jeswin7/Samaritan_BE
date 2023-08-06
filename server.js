4b9b78c6ae54af793f5dbaa6c47b6f225f8cda53
jeswin7 <jjohncy8891@conestogac.on.ca>
2023-08-05 22:29:57 -0400

    Message fetch based on sender & receiver IDs API - bug fix

--- server.js
+++ server.js
@@ -754,7 +754,7 @@ app.get('/getMessages', (req, res) => {
     receiver
   } = req.query;
 
-  let sql = `SELECT * FROM COMMUNICATION WHERE SENDER=${sender}`
+  let sql = `SELECT * FROM COMMUNICATION WHERE SENDER=${sender} AND RECEIVER=${receiver}`
 
   db.query(sql, (err, result) => {
     if(err) throw(err);
